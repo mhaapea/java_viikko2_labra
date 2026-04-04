4aa50a4c7698dd50d306edfa95769dbb93e6011f
mhaapea <mihaapea@gmail.com>
2026-04-04 14:09:10 +0000

    Task 6 ja labra valmis

--- Task06_PackagesVisibility/src/fi/viikko2/task06/sub/SportsCar.java
+++ Task06_PackagesVisibility/src/fi/viikko2/task06/sub/SportsCar.java
@@ -20,11 +20,19 @@ package fi.viikko2.task06.sub;
 import fi.viikko2.task06.model.Vehicle;
 
 public class SportsCar extends Vehicle {
+
+    int horsepower;
+
+    public SportsCar(String brand, int mileage, int horsepower) {
+        super(brand, 0);
+        this.horsepower = horsepower;
+    }
    
 
 
     @Override
     public String toString(){ 
         //  Toteuta ja palauta merkkijono. Kun olet valmis, POISTA alla oleva rivi.
-        throw new UnsupportedOperationException("TODO: implement toString()"); }
+        return "Sportscar: brand = " + brand + ", horsepower = " + horsepower;
+    }
 }
